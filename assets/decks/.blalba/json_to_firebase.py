3625690fecea0e8fd81660d99ed97467a624ca04
hng011 <hansnaufal.3040@gmail.com>
2025-08-02 11:40:19 +0700

    refactor fs schme

--- assets/decks/.blalba/json_to_firebase.py
+++ assets/decks/.blalba/json_to_firebase.py
@@ -1,27 +1,33 @@
 import os
 import json
-import firebase_admin
 import time
 from dotenv import load_dotenv
+import firebase_admin
 from firebase_admin import credentials, firestore
 
 load_dotenv()
 
-cred = credentials.Certificate(os.getenv("CRED_FIREBASE"))
-firebase_admin.initialize_app(cred)
 
 firestore_coll_name = os.getenv("FIRESTORE_COLL_NAME")
 json_file = "deck.json"
 
+print("Connecting fs")
+cred = credentials.Certificate(os.getenv("CRED_FIREBASE"))
+firebase_admin.initialize_app(cred)
 db = firestore.client()
+print("connected")
 
 if __name__ == "__main__":
     try:
         s = time.time()
         print("Start uploading")
+
         with open(json_file, "r") as f:
-            data = json.load(f)            
-        db.collection(firestore_coll_name).add(data)
+            data: dict = json.load(f)           
+            
+            for i in data["listDeck"]:            
+                db.collection(firestore_coll_name).document(i).set({"content": data["listDeck"][i]})
+                
         print(f"Completed in {time.time() - s:.8f}s")
     except Exception as E:
         print(E)
